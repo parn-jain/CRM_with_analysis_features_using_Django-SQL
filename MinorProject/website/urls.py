d7e027693ca4bb6dad66591c73b4ded706bd46dd
“parn” <pjisvgreat@gmail.com>
2023-10-08 15:07:08 +0530

    Added login and logout functionalities

---
+++ MinorProject/website/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include   
+from website import views
+
+urlpatterns = [
+
+    path('',views.index, name='home'),
+    # path('home',views.index, name='home'),
+    path('about',views.about, name='about'),
+    path('contact',views.contactus, name='about'),
+    path('services',views.services, name='services'),
+    # path('login',views.login_user, name='login'),
+    path('logout',views.logout_user, name='logout'),
+]
--- MinorProject/website/views.py
+++ MinorProject/website/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+def index(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        #Authinticate
+        user = authenticate(request,username = username, password = password)
+        if user is not None:
+            login(request,user)
+            messages.success(request, "You have been logged in")
+            return redirect('home')
+        else:
+            messages.success(request,"Threr was an error")
+            return redirect('home')
+    else:
+        context = {'variable':'this was a variable'}
+        return render(request, "index.html",context)
+    # return HttpResponse("This is home page")
+
+def about(request):
+    return render(request, "about.html")
+
+def contactus (request):
+    return render(request, "contact.html")
+
+def services (request):
+    return render(request, "services.html")
+
+def login_user(request):
+    pass
+def logout_user(request):
+    logout(request)
+    messages.success(request,"You have been loged out")
+    return redirect('home')
